692e9374e03559a2218c33572388c242d184394a
Nathan Strang <n.strang95@gmail.com>
2024-01-09 17:23:21 +0000

    Adding program code

---
+++ Switches/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Switches
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Switches are an efficient alternative to lots of if statements
+
+            Console.WriteLine("What day is it today?");
+            String day = Console.ReadLine();
+
+            switch (day)
+            {
+                case "Monday":
+                    Console.WriteLine("It's Monday!");
+                    break;
+                case "Tuesday":
+                    Console.WriteLine("It's Tuesday!");
+                    break;
+                case "Wednesday":
+                    Console.WriteLine("It is Wednesday, my dude!");
+                    break;
+                case "Thursday":
+                    Console.WriteLine("It's Thursday!");
+                    break;
+                case "Friday":
+                    Console.WriteLine("It's Friday!");
+                    break;
+                case "Saturday":
+                    Console.WriteLine("It's Saturday!");
+                    break;
+                case "Sunday":
+                    Console.WriteLine("It's Sundaay!");
+                    break;
+                 default:
+                    Console.WriteLine(day + " is not a day!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
